6cbb668075706ff68ec58d472cef483ddbc686df
Mitchell <50689806+kyle1an@users.noreply.github.com>
2021-03-01 21:45:26 +0800

    Update Chat.js

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -17,8 +17,8 @@ export const Chat = ({location}) => {
     const [users, setUsers] = useState("");
     const [message, setMessage] = useState("");
     const [messages, setMessages] = useState([]);
-    // const ENDPOINT = "https://react-chat-page.herokuapp.com/";
-    const ENDPOINT = "https://tchell-demo.herokuapp.com/";
+    const ENDPOINT = "https://react-chat-page.herokuapp.com/";
+    // const ENDPOINT = "https://tchell-demo.herokuapp.com/";
 
     useEffect(() => {
         const {name, room} = queryString.parse(location.search);
