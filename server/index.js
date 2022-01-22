e901fee2acd508a2cf85cf7404ce9007cb246ed8
Mitchell <50689806+kyle1an@users.noreply.github.com>
2022-01-22 20:54:49 +0800

    port chg

--- server/index.js
+++ server/index.js
@@ -63,6 +63,6 @@ io.on("connect", (socket) => {
     });
 });
 
-server.listen(process.env.PORT || 5000, () =>
+server.listen(process.env.PORT || 5001, () =>
     console.log(`Server has started.`)
 );
